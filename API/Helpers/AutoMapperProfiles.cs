b645295b82b9af5978d744d72230a6db6c2b875e
Vijay Babar <babarvm@gmail.com>
2025-07-28 00:35:50 +0530

    End of section 15: Adding the messaging feature

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -22,5 +22,11 @@ public class AutoMapperProfiles : Profile
         CreateMap<RegisterDto, AppUser>();
 
         CreateMap<string, DateOnly>().ConvertUsing(s => DateOnly.Parse(s));
+
+        CreateMap<Message, MessageDto>()
+            .ForMember(d => d.SenderPhotoUrl,
+                o => o.MapFrom(s => s.Sender.Photos.FirstOrDefault(x => x.IsMain)!.Url))
+            .ForMember(d => d.RecipientPhotoUrl,
+                o => o.MapFrom(s => s.Recipient.Photos.FirstOrDefault(x => x.IsMain)!.Url));
     }
 }
